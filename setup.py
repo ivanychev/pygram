dc981fbe973a4b6bdb6579db59d66d4e2bbc584e
Sergey Ivanchev <sergeyivanychev@gmail.com>
2017-01-20 20:26:40 +0300

    Fixed setup.py to follow pep8

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'pygram',
-  packages = ['pygram'],
-  version = '0.0.1a',
-  description = 'An instagram + python magic',
-  author = 'Sergey Ivanychev',
-  author_email = 'sergeyivanychev@gmail.com',
-  url = 'https://github.com/ivanychev/pygram',
-  download_url = 'https://github.com/ivanychev/pygram/tarball/0.0.1a',
-  keywords = ['insta', 'instagram'],
-  classifiers = [],
+  name='pygram',
+  packages=['pygram'],
+  version='0.0.1a',
+  description="An instagram + python magic",
+  author='Sergey Ivanychev',
+  author_email='sergeyivanychev@gmail.com',
+  url='https://github.com/ivanychev/pygram',
+  download_url='https://github.com/ivanychev/pygram/tarball/0.0.1a',
+  keywords=['insta', 'instagram'],
+  classifiers=[],
 )
