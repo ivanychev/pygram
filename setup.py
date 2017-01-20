70009a432139c3951c76f454ed13e30b3f691f41
Sergey Ivanchev <sergeyivanychev@gmail.com>
2017-01-20 19:20:23 +0300

    Preparations to PyPI publication

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pygram',
+  packages = ['pygram'],
+  version = '0.0.1a',
+  description = 'An instagram + python magic',
+  author = 'Sergey Ivanychev',
+  author_email = 'sergeyivanychev@gmail.com',
+  url = 'https://github.com/ivanychev/pygram',
+  download_url = 'https://github.com/ivanychev/pygram/0.0.1a',
+  keywords = ['insta', 'instagram'],
+  classifiers = [],
+)
